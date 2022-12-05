5c791aa5151aa9c6f0d0d701b8762b01d6e82be3
IUT BLAGNAC <noreply@univ-tlse2.fr>
2022-12-05 15:10:59 +0100

    Esteban ajout formulaire incription connexion

---
+++ Site/src/formulaireConnexion.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="formulaire.css">
+    <title>Formulaire de connexion</title>
+</head>
+<body>
+    <div class="box">
+        <div class="form">  
+            <h2>Veuillez vous <p>connecter</p></h2>
+            <div class="inputBox">
+                <input type="text" required="Requis">
+                <span>Nom d'utilisateur</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input type="password" required>
+                <span>Mot de passe</span>
+                <i></i>
+            </div>
+            <div class="links">
+                <a href="formulaireInscription.php"> Vous n'avez pas de compte ? Inscrivez-vous.</a>
+            </div>
+            <input type="submit" value="Se connecter">
+        </div>
+    </div>
+
+</body>
+</html>
