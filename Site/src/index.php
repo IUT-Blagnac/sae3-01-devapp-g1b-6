b5c7001ecb708ec77fbca4fe03745f96002c6de2
EnzoMancini <95383732+EnzoMancini@users.noreply.github.com>
2022-12-05 16:04:45 +0100

    Commit Enzo 05/12/2022 16:04

--- Site/src/index.php
+++ Site/src/index.php
@@ -2,12 +2,13 @@
     <head>
         <meta charset="utf-8"/>
         <title>SubOne Accueil</title>
+        <link rel="shortcut icon" href="images/Logo_SubOne_png.png" />
         <link rel="stylesheet" href="include/CSS-index.css">
     </head>
     <body>
         <div id="barreHaut">
             <div id= "logo">
-                
+                <img src="images/Logo_SubOne_png.png">
             </div>
         </div>
     </body>
