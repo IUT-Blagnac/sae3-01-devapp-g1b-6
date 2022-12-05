0b20a3462f8659f819ee6f5ae53cf2ca1b9d987b
Enzo Mancini <95383732+EnzoMancini@users.noreply.github.com>
2022-12-05 13:22:35 +0100

    Update footer.php

--- Site/src/include/footer.php
+++ Site/src/include/footer.php
@@ -1,1 +1,7 @@
-
+<footer id="bottom">
+    <nav style="" class="navbar navbar-dark bg-dark shadow-sm fixed-bottom">
+        <div class="inner" style="text-align: center; width: 100%">
+            <a class="nav-item" style="font-size: 15px" href="#">Pour plus d'information n'hésitez pas à nous contacter !</a>
+        </div>
+    </nav>
+</footer>
