630727a81726fa554abd7ab94e1f61394cc57c51
EnzoMancini <95383732+EnzoMancini@users.noreply.github.com>
2022-12-06 14:39:10 +0100

    Enzo

--- Site/src/include/header.php
+++ Site/src/include/header.php
@@ -1,6 +1,3 @@
-<link rel="stylesheet" href="./style.css">
-<nav style="" class="navbar navbar-dark bg-dark shadow-sm fixed-top">
-    <div style="text-align: center; width: 100%">
-        <a href="" class="bouton5">Compte client</a>
-    </div>
-</nav>
+<div class="header">
+    <link rel="stylesheet" href="style.css">
+</div>
--- Site/src/index.php
+++ Site/src/index.php
@@ -7,6 +7,5 @@
     </head>
     <body>
         <?php include("./include/header.php");?>
-        <?php include("./include/footer.php");?>
     </body>
 </html>
