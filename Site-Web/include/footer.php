08aaa4719267f8f6451c5e9361afefead66d7bf9
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2022-12-17 16:09:40 +0100

    Add files via upload

---
+++ Site-Web/include/footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="include/footer.css">
+<div id="bottom">
+	<div id="divf">
+		© 2022-2023, SubOne.com
+	</div>
+</div>
---
+++ Site-Web/include/header.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="include/header.css">
+<div id="header">
+	<logo>
+		<a href="index.php"><img src="include/logo.png" alt="Image logo" id="logo"></a>
+	</logo>
+	<bouton>
+		<a href="compteClient.php" ><button type="button" class="bouton" id="bouton1">Compte client</button></a>
+		<a href="contacts.php"><button type="button" class="bouton" id="bouton1">Contact</button></a>
+		<a href="panier.php"><button type="button" class="bouton"><img src="include/caddie.png" width="30px" height="30px"></button></a>
+		<a href="formulaireConnexion.php"><button type="button" class="bouton" id="bouton1">Connexion</button></a>
+	</bouton>
+</div>
