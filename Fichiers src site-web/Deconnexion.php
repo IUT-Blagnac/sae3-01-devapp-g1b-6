9b598475f080debcc3a6b1b5ec66d16535b06fdc
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2023-01-19 13:45:42 +0100

    Delete Fichiers src site-web directory

--- Fichiers src site-web/Deconnexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['access']='NOT OK';
-  echo '<script language="JavaScript" type="text/javascript"> 
-						alert("D\u00e9connexion effectu\u00e9e !"); 
-						location.href = "./index.php";
-					</script>';
-	
-?>
