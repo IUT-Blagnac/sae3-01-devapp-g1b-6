431391c3824243135c843409af06b09e70a2244f
Esteban BIRET-TOSCANO <100210730+EstebanBiret@users.noreply.github.com>
2023-01-19 13:52:56 +0100

    Add files via upload

---
+++ Fichiers src site-web/include/footer3.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+
+<link rel="stylesheet" href="include/footer3.css">
+
+<body>
+    <div class="footer">
+        
+        &#169 2022-2023, SubOne.com
+    </div>
+</body>
+</html>
